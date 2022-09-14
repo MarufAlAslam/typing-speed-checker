16ce640acd4e3c648874c8898ad42835565b2f30
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-09-15 00:31:46 +0600

    issue 6: time fraction issue fixed

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = Math.floor((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
